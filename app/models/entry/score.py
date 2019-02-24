0da472cdd78477dc248b86d4d87292e513dabb15
cendeal <1798062051@qq.com>
2019-02-24 15:07:50 +0800

    更改学分计算

--- app/models/entry/score.py
+++ app/models/entry/score.py
@@ -30,11 +30,11 @@ class Score(DataBase):
     def calculate_all(source_list):
         data = {'per_gpa': 0,
                 'per_percentile': 0,
-                'total_credit': 0,
-                'required_credit': 0,
-                'selected_major_credit': 0,
-                'public_credit': 0,
-                'effective_total': 0
+                'total_credit': [0, 0],
+                'required_credit': [0, 0],
+                'selected_major_credit': [0, 0],
+                'public_credit': [0, 0],
+                'all': [0, 0]
                 }
         total = source_list['total']
         gpa = 0
@@ -42,16 +42,20 @@ class Score(DataBase):
         for i in source_list['list']:
             gpa += float(i['gpa'])
             percentile += int(i['percentile'])
-
             data['total_credit'] += float(i['credit'])
-            if i['percentile'] >= 60:
-                data['effective_total'] += 1
-                if i['type'] == '06':
-                    data['public_credit'] += float(i['credit'])
-                elif i['type'] == '01':
-                    data['required_credit'] += float(i['credit'])
-                elif i['type'] == '03':
-                    data['selected_major_credit'] += float(i['credit'])
+            flag = True if float(i['credit']) >= 60 else False
+            index = 0
+            if flag:
+                data['all'][index] += 1
+            else:
+                index = 1
+                data['all'][index] += 1
+            if i['type'] == '06':
+                data['public_credit'][index] += float(i['credit'])
+            elif i['type'] == '01':
+                data['required_credit'][index] += float(i['credit'])
+            elif i['type'] == '03':
+                data['selected_major_credit'][index] += float(i['credit'])
         if total is not 0:
             data['per_percentile'] = percentile // total
             data['per_gpa'] = round(gpa / total, 2)
