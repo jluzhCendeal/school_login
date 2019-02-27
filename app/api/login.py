80c3a6a2fde389e5240a65c6820157ee8133b77c
cendeal <1798062051@qq.com>
2019-02-27 17:00:23 +0800

    新增token登陆,修改cookie的user为token

--- app/api/login.py
+++ app/api/login.py
@@ -7,19 +7,27 @@ from . import api
 
 @api.route('/login', methods=['post', 'get'])
 def login():
-    yhm = request.values.get('yhm', '')
-    mm = request.values.get('mm', '')
+    yhm = request.values.get('yhm')
+    mm = request.values.get('mm')
+    token = request.values.get('token')
+    data = {'msg': '未登录！', 'code': '-1', 'data': {}}
+    if (yhm is None or mm is None) and token is not None:
+        from app.models.entry.student import Student
+        user = Student(token)
+        yhm = user.yhm
+        mm = user.mm
+
     from app.utils.stulogin_manager import LoginService
     loginService = LoginService()
     flag = loginService.login(yhm, mm)
-    data = {'msg': '未登录！', 'code': '-1', 'data': {}}
     if flag:
         data['code'] = 0
         data['msg'] = '已登录！'
         user = base64.b64encode((yhm + '\n' + mm).encode()).decode()
-        data['data'] = {'user': user}
+        data['data'] = {'token': user}
         resp = make_response(JsonpFormat.callback(data))
-        resp.set_cookie('user', user)
+        resp.set_cookie('token', user)
         loginService.logout()
         return resp
+
     return JsonpFormat.callback(data)
